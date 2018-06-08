d8044f058f5fa5f61ce29582009d40328bfe5ddb
Anders Aaberg <aaa@nine.dk>
2018-06-08 15:50:20 +0200

    Created fresh geb project using: curl -O start.grails.org/grails-less-test.zip -d version=3.3.5 -d features=geb2

---
+++ README.md
@@ -0,0 +1,1 @@
+Generated using: curl -O start.grails.org/grails-less-test.zip -d version=3.3.5 -d features=geb2
---
+++ grails-app/init/grails/less/test/BootStrap.groovy
@@ -0,0 +1,9 @@
+package grails.less.test
+
+class BootStrap {
+
+    def init = { servletContext ->
+    }
+    def destroy = {
+    }
+}
